1d4ef46f5e17ad2a9f61f932403f40be75f1f305
hicham-2 <aissaoui.hicham00@gmail.com>
2021-11-17 09:37:12 +0100

    test1

--- src/exoIntegrationQuestion/Main.java
+++ src/exoIntegrationQuestion/Main.java
@@ -9,8 +9,105 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Themes leThemes = new Themes();
 		try {
-			Question lesQuestion = new QCM("A?",2,new String[] {"A","B","C"},1);
+			Question lesQuestion = new QCM("Question1_Theme1",1,new String[] {"A","B","C"},1);
+			
+			Question lesQuestion2 = new QCM("Question2_Theme1",1,new String[] {"A","B","C"},2);
+			Question lesQuestion3 = new QCM("Question3_Theme1",1,new String[] {"A","B","C"},3);
+			Question lesQuestion4 = new QCM("Question1_Theme2",1,new String[] {"A","B","C"},3);
+			Question lesQuestion5 = new QCM("Question2_Theme2",1,new String[] {"A","B","C"},1);
+			Question lesQuestion6 = new QCM("Question3_Theme2",1,new String[] {"A","B","C"},2);
+			Question lesQuestion7 = new QCM("Question1_Theme3",1,new String[] {"A","B","C"},1);
+			Question lesQuestion8 = new QCM("Question2_Theme3",1,new String[] {"A","B","C"},1);
+			Question lesQuestion9 = new QCM("Question3_Theme3",1,new String[] {"A","B","C"},1);
+			Question lesQuestion10 = new QCM("Question1_Theme4",1,new String[] {"A","B","C"},2);
+			Question lesQuestion11= new QCM("Question2_Theme4",1,new String[] {"A","B","C"},1);
+			Question lesQuestion12= new QCM("Question3_Theme4",1,new String[] {"A","B","C"},3);
+			Question lesQuestion13= new QCM("Question1_Theme5",1,new String[] {"A","B","C"},1);
+			Question lesQuestion14= new QCM("Question2_Theme5",1,new String[] {"A","B","C"},1);
+			Question lesQuestion15= new QCM("Question3_Theme5",1,new String[] {"A","B","C"},2);
+			Question lesQuestion16= new QCM("Question1_Theme6",1,new String[] {"A","B","C"},3);
+			Question lesQuestion17= new QCM("Question2_Theme6",1,new String[] {"A","B","C"},1);
+			Question lesQuestion18= new QCM("Question3_Theme6",1,new String[] {"A","B","C"},1);
+			Question lesQuestion19= new QCM("Question1_Theme7",1,new String[] {"A","B","C"},2);
+			Question lesQuestion20= new QCM("Question2_Theme7",1,new String[] {"A","B","C"},1);
+			Question lesQuestion21= new QCM("Question3_Theme7",1,new String[] {"A","B","C"},3);
+			Question lesQuestion22= new QCM("Question1_Theme8",1,new String[] {"A","B","C"},1);
+			Question lesQuestion23= new QCM("Question2_Theme8",1,new String[] {"A","B","C"},3);
+			Question lesQuestion24= new QCM("Question3_Theme8",1,new String[] {"A","B","C"},1);
+			Question lesQuestion25= new QCM("Question1_Theme9",1,new String[] {"A","B","C"},2);
+			Question lesQuestion26= new QCM("Question2_Theme9",1,new String[] {"A","B","C"},1);
+			Question lesQuestion27= new QCM("Question1_Theme9",1,new String[] {"A","B","C"},3);
+			Question lesQuestion28= new QCM("Question2_Theme10",1,new String[] {"A","B","C"},1);
+			Question lesQuestion29= new QCM("Question3_Theme10",1,new String[] {"A","B","C"},2);
+			Question lesQuestion30 = new QCM("Question1_Theme10",1,new String[] {"A","B","C"},1);
+/*************************************************************************************/
+			
+
+			Question lesQuestion = new QCM("Question1_Theme1",2,new String[] {"A","B","C"},1);
+			Question lesQuestion202 = new QCM("Question2_Theme1",2,new String[] {"A","B","C"},2);
+			Question lesQuestion203 = new QCM("Question3_Theme1",2,new String[] {"A","B","C"},3);
+			Question lesQuestion204 = new QCM("Question1_Theme2",2,new String[] {"A","B","C"},3);
+			Question lesQuestion205 = new QCM("Question2_Theme2",2,new String[] {"A","B","C"},1);
+			Question lesQuestion206 = new QCM("Question3_Theme2",2,new String[] {"A","B","C"},2);
+			Question lesQuestion207 = new QCM("Question1_Theme3",2,new String[] {"A","B","C"},1);
+			Question lesQuestion208 = new QCM("Question2_Theme3",2,new String[] {"A","B","C"},1);
+			Question lesQuestion209= new QCM("Question3_Theme3",2,new String[] {"A","B","C"},1);
+			Question lesQuestion210 = new QCM("Question1_Theme4",2,new String[] {"A","B","C"},2);
+			Question lesQuestion211= new QCM("Question2_Theme4",2,new String[] {"A","B","C"},1);
+			Question lesQuestion212= new QCM("Question3_Theme4",2,new String[] {"A","B","C"},3);
+			Question lesQuestion213= new QCM("Question1_Theme5",2,new String[] {"A","B","C"},1);
+			Question lesQuestion214= new QCM("Question2_Theme5",2,new String[] {"A","B","C"},1);
+			Question lesQuestion215= new QCM("Question3_Theme5",2,new String[] {"A","B","C"},2);
+			Question lesQuestion216= new QCM("Question1_Theme6",2,new String[] {"A","B","C"},3);
+			Question lesQuestion217= new QCM("Question2_Theme6",2,new String[] {"A","B","C"},1);
+			Question lesQuestion218= new QCM("Question3_Theme6",2,new String[] {"A","B","C"},1);
+			Question lesQuestion219= new QCM("Question1_Theme7",2,new String[] {"A","B","C"},2);
+			Question lesQuestion220= new QCM("Question2_Theme7",2,new String[] {"A","B","C"},1);
+			Question lesQuestion221= new QCM("Question3_Theme7",2,new String[] {"A","B","C"},3);
+			Question lesQuestion222= new QCM("Question1_Theme8",2,new String[] {"A","B","C"},1);
+			Question lesQuestion223= new QCM("Question2_Theme8",2,new String[] {"A","B","C"},3);
+			Question lesQuestion224= new QCM("Question3_Theme8",2,new String[] {"A","B","C"},1);
+			Question lesQuestion225= new QCM("Question1_Theme9",2,new String[] {"A","B","C"},2);
+			Question lesQuestion226= new QCM("Question2_Theme9",2,new String[] {"A","B","C"},1);
+			Question lesQuestion227= new QCM("Question1_Theme9",2,new String[] {"A","B","C"},3);
+			Question lesQuestion228= new QCM("Question2_Theme10",2,new String[] {"A","B","C"},1);
+			Question lesQuestion229= new QCM("Question3_Theme10",2,new String[] {"A","B","C"},2);
+			Question lesQuestion230 = new QCM("Question1_Theme10",2,new String[] {"A","B","C"},1);
+
+/*************************************************************************************/
+			Question lesQuestion = new QCM("Question1_Theme1",3,new String[] {"A","B","C"},1);
+			Question lesQuestion302 = new QCM("Question2_Theme1",3,new String[] {"A","B","C"},2);
+			Question lesQuestion303 = new QCM("Question3_Theme1",3,new String[] {"A","B","C"},3);
+			Question lesQuestion304 = new QCM("Question1_Theme2",3,new String[] {"A","B","C"},3);
+			Question lesQuestion305 = new QCM("Question2_Theme2",3,new String[] {"A","B","C"},1);
+			Question lesQuestion306 = new QCM("Question3_Theme2",3,new String[] {"A","B","C"},2);
+			Question lesQuestion307 = new QCM("Question1_Theme3",3,new String[] {"A","B","C"},1);
+			Question lesQuestion308 = new QCM("Question2_Theme3",3,new String[] {"A","B","C"},1);
+			Question lesQuestion309= new QCM("Question3_Theme3",3,new String[] {"A","B","C"},1);
+			Question lesQuestion310 = new QCM("Question1_Theme4",3,new String[] {"A","B","C"},2);
+			Question lesQuestion311= new QCM("Question2_Theme4",3,new String[] {"A","B","C"},1);
+			Question lesQuestion312= new QCM("Question3_Theme4",3,new String[] {"A","B","C"},3);
+			Question lesQuestion313= new QCM("Question1_Theme5",3,new String[] {"A","B","C"},1);
+			Question lesQuestion314= new QCM("Question2_Theme5",3,new String[] {"A","B","C"},1);
+			Question lesQuestion315= new QCM("Question3_Theme5",3,new String[] {"A","B","C"},2);
+			Question lesQuestion316= new QCM("Question1_Theme6",3,new String[] {"A","B","C"},3);
+			Question lesQuestion317= new QCM("Question2_Theme6",3,new String[] {"A","B","C"},1);
+			Question lesQuestion318= new QCM("Question3_Theme6",3,new String[] {"A","B","C"},1);
+			Question lesQuestion319= new QCM("Question1_Theme7",3,new String[] {"A","B","C"},2);
+			Question lesQuestion320= new QCM("Question2_Theme7",3,new String[] {"A","B","C"},1);
+			Question lesQuestion321= new QCM("Question3_Theme7",3,new String[] {"A","B","C"},3);
+			Question lesQuestion322= new QCM("Question1_Theme8",3,new String[] {"A","B","C"},1);
+			Question lesQuestion323= new QCM("Question2_Theme8",3,new String[] {"A","B","C"},3);
+			Question lesQuestion324= new QCM("Question3_Theme8",3,new String[] {"A","B","C"},1);
+			Question lesQuestion325= new QCM("Question1_Theme9",3,new String[] {"A","B","C"},2);
+			Question lesQuestion326= new QCM("Question2_Theme9",3,new String[] {"A","B","C"},1);
+			Question lesQuestion327= new QCM("Question1_Theme9",3,new String[] {"A","B","C"},3);
+			Question lesQuestion328= new QCM("Question2_Theme10",3,new String[] {"A","B","C"},1);
+			Question lesQuestion329= new QCM("Question3_Theme10",3,new String[] {"A","B","C"},2);
+			Question lesQuestion330 = new QCM("Question1_Theme10",3,new String[] {"A","B","C"},1);
+
 			
 		} catch (OutOfRangeException e) {
 			// TODO Auto-generated catch block
